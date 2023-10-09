f6c0cb8c997d936742a770ae51452e1818f33faa
gividal <gimenavidal10@gmail.com>
2023-10-09 17:10:53 -0300

    nuevoscambios

--- extensionjava.js
+++ extensionjava.js
@@ -1,32 +1,54 @@
 alert ("¡Bienvenidos a Turismo Ingenias! Que disfrutes el recorrido por nuestro sitio.")
-let correo = prompt ("Si desea recibir novedades de nuestras ofertas de paquetes turisticos ingrese su correo:")
-console.log(correo, "correo de usuario");
-let nombre = prompt (" Ingrese su nombre:")
-console.log(nombre, "nombre de usuario");
-let destino = prompt (" Ingrese el destino del cual quiere recibir novedades:")
-console.log(destino, "destino de interes");
-alert ("¡Gracias! Esperamos que nuestras ofertas y recomendaciones sean de su interés.")
 
-let ayudaalusuario = prompt ("¿Necesitas ayuda para elegir tu próximo destino? SI - NO ");
-if (ayudaalusuario == "SI"){
-    alert ("A continuacion te haremos un pequeño test para recomendarte algunos destinos.");
-} else if (ayudaalusuario == "NO") {
-    console.log("No se mostrara el mensaje");
-} else {
-    alert("Usted no ingreso una opción correcta")
-}
+sessionStorage.setItem ('correo','ricardo@gmail.com')
+sessionStorage.setItem('nombre', 'Ricardo')
+sessionStorage.setItem('destino', 'Chile, Islas de Pascua')
 
-let pregunta1 = prompt ("Para ti “vacaciones” es sinónimo de…   A) Relax y descanso   B) Deporte y aventura   C) Nuevas culturas y costumbres ")
-console.log(pregunta1, "RespuestaPregunta1");
+let correo = sessionStorage.getItem("correo") 
+let nombre = sessionStorage.getItem("nombre")
+let destino = sessionStorage.getItem("destino")
 
-let pregunta2 = prompt ("¿Qué preferirías hacer en tu viaje?  A) Visitar un museo o ver un espectáculo   B) Practicar algún deporte de aventura o extremo   C) Un masaje o un ratito en el spa sería fantástico")
-console.log(pregunta2, "RespuestaPregunta2");
+console.log(nombre);
+console.log(correo);
+console.log(destino);
 
-let pregunta3 = prompt ("¿Qué te define mejor?  A) Intelectual y amante del arte   B) Atrevido, me gusta vivir emociones fuertes   C) Tolerante y de mente abierta")
-console.log(pregunta3, "RespuestaPregunta3");
+alert("Tus últimos datos ingresados:  Nombre: " + nombre + " - " + "Correo: " + correo + " - " + "Destino de interés: " + destino)
 
-if (pregunta1 == "A" && pregunta2 == "C" && pregunta3 == "C"){
-    alert ( "Los destinos recomendados son: 1) Isla de Pascua (Chile)   2) Lagunas Cejar y de Baltinache (Chile)   3) Lago Moreno (Arg)")
-} else {
-    prompt ("Le enviaremos los resultados. Ingrese su correo:")
-}
+
+
+
+let consultasNovedades = 
+`{
+    "nombre":"Roberto",
+    "correo":"roberto@gmail.com",
+    "destinoDeInteres":"San Carlos de Bariloche"
+},
+{
+    "nombre":"Andrea",
+    "correo":"andrea@gmail.com",
+    "destinoDeInteres":"Cataratas del Iguazu"
+    
+}`
+    
+let jsonconsultasconvertidas = JSON.parse(consultasNovedades);
+console.log(jsonconsultasconvertidas);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
